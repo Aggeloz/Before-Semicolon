317e86f89e32f48714c768b7cdc97f8ec6454522
ecorreia <el4591@hotmail.com>
2018-09-17 04:55:04 -0400

    move to folders

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
 (function () {
   
-  // const ellipseOverflowedText = selector => {
-  //   const el = document.querySelector(selector);
-  //   const wordArray = el.textContent.split(' ');
-  //
-  //   while (el.scrollHeight > el.offsetHeight) {
-  //     wordArray.pop();
-  //     el.textContent = wordArray.join(' ') + '...';
-  //   }
-  // };
-  //
-  // ellipseOverflowedText('.info-navigator h2');
-  //
-  // window.addEventListener('resize', () => {
-  //   ellipseOverflowedText('.info-navigator h2');
-  // })
-  
 })()
--- src/index.js
+++ src/index.js
@@ -1,22 +1,4 @@
 "use strict";
 
-(function () {
-
-  // const ellipseOverflowedText = selector => {
-  //   const el = document.querySelector(selector);
-  //   const wordArray = el.textContent.split(' ');
-  //
-  //   while (el.scrollHeight > el.offsetHeight) {
-  //     wordArray.pop();
-  //     el.textContent = wordArray.join(' ') + '...';
-  //   }
-  // };
-  //
-  // ellipseOverflowedText('.info-navigator h2');
-  //
-  // window.addEventListener('resize', () => {
-  //   ellipseOverflowedText('.info-navigator h2');
-  // })
-
-})();
+(function () {})();
 //# sourceMappingURL=index.js.map
