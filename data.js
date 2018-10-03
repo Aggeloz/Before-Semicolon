5001c4b209d09c74488c465cfda2130c9ae822bf
ecorreia <el4591@hotmail.com>
2018-10-03 00:14:03 -0400

    github post gifs and data updated

--- data.js
+++ data.js
@@ -1,110 +1,163 @@
+const POST_TYPES = {
+	YOUTUBE: 'youtube',
+	GITHUB: 'github'
+}
+
 const posts = [
 	{
-		id: 0,
 		videoId: "XnndhSaszuY",
 		title: "Search Field (animate search icon into text cursor) - UI [How To]",
 		thumbnailPath: "",
-		url: "https://www.youtube.com/embed/XnndhSaszuY",
+		url: "https://youtu.be/XnndhSaszuY",
 		description: "Learn how to make your search field magical by turning your search icon into a text cursor when the user clicks on the field.",
-		type: "youtube"
+		type: POST_TYPES.YOUTUBE
 	},
 	{
-		id: 1,
 		videoId: "o9M9GvwOSCk",
 		title: "How to Query/Select DOM Element with vanilla javascript - (You Don't Need jQuery)",
 		thumbnailPath: "",
-		url: "https://www.youtube.com/embed/o9M9GvwOSCk",
+		url: "https://youtu.be/o9M9GvwOSCk",
 		description: "Learn how to query any dom element without needing the awesome jQuery DOM traversing tools.",
-		type: "youtube"
+		type: POST_TYPES.YOUTUBE
 	},
 	{
-		id: 2,
 		videoId: "xDFNvEwPYYY",
 		title: "When to use HTML image tag vs CSS background image",
 		thumbnailPath: "",
-		url: "https://www.youtube.com/embed/xDFNvEwPYYY",
+		url: "https://youtu.be/xDFNvEwPYYY",
 		description: "A quick overview of the best practice when it comes to putting an image in your page",
-		type: "youtube"
+		type: POST_TYPES.YOUTUBE
 	},
 	{
-		id: 3,
 		videoId: "ysS1U1zzF6Y",
 		title: "CSS Specificity - What style rules gets set?",
 		thumbnailPath: "",
-		url: "https://www.youtube.com/embed/ysS1U1zzF6Y",
+		url: "https://youtu.be/ysS1U1zzF6Y",
 		description: "Learn all about CSS specificity",
-		type: "youtube"
+		type: POST_TYPES.YOUTUBE
 	},
 	{
-		id: 4,
 		videoId: "WJ3lpPVyVUI",
 		title: "Remove, Clone and Replace DOM Elements [ How To ]",
 		thumbnailPath: "",
-		url: "https://www.youtube.com/embed/WJ3lpPVyVUI",
+		url: "https://youtu.be/WJ3lpPVyVUI",
 		description: "Learn how to remove, clone and replace dom elements with plain/vanilla javascript",
-		type: "youtube"
+		type: POST_TYPES.YOUTUBE
 	},
 	{
-		id: 5,
 		videoId: "BRdq03yqDhY",
 		title: "Select Elements Siblings [ How To ]",
 		thumbnailPath: "",
-		url: "https://www.youtube.com/embed/BRdq03yqDhY",
+		url: "https://youtu.be/BRdq03yqDhY",
 		description: "Learn how to select DOM element siblings",
-		type: "youtube"
+		type: POST_TYPES.YOUTUBE
 	},
 	{
-		id: 6,
 		videoId: "mXOGpcm19J8",
 		title: "Select any DOM element and (Nodelist vs HTMLCollection) - [ How to ]",
 		thumbnailPath: "",
-		url: "https://www.youtube.com/embed/mXOGpcm19J8",
+		url: "https://youtu.be/mXOGpcm19J8",
 		description: "Learn how to get element by ID, ClassName, Tag Name, name and by querying like css",
-		type: "youtube"
+		type: POST_TYPES.YOUTUBE
 	},
 	{
-		id: 7,
 		videoId: "sILWThH3RnA",
 		title: "Select, Remove, Replace and Clone DOM Elements with plain Javascript",
 		thumbnailPath: "",
-		url: "https://www.youtube.com/embed/sILWThH3RnA",
+		url: "https://youtu.be/sILWThH3RnA",
 		description: "Learn how to select remove replace or clone any element, any child node, parent node, any siblings node using plain Javascript.",
-		type: "youtube"
+		type: POST_TYPES.YOUTUBE
 	},
 	{
-		id: 8,
 		videoId: "swWeWesZVZU",
 		title: "When to use Section vs Article vs Div in Html?",
 		thumbnailPath: "",
-		url: "https://www.youtube.com/embed/swWeWesZVZU",
+		url: "https://youtu.be/swWeWesZVZU",
 		description: "Learn about the semantics behind section article and div tags in HTML.",
-		type: "youtube"
+		type: POST_TYPES.YOUTUBE
 	},
 	{
-		id: 9,
 		videoId: "bpdn9bwC2_o",
 		title: "Lossy vs Lossless",
 		thumbnailPath: "",
-		url: "https://www.youtube.com/embed/bpdn9bwC2_o",
+		url: "https://youtu.be/bpdn9bwC2_o",
 		description: "A quick overview of how lossy and lossless formats play a role in our daily tech use and what they are. The video is a simple, straight format explanation where lossy and lossless format can be found or created and used.",
-		type: "youtube"
+		type: POST_TYPES.YOUTUBE
 	},
 	{
-		id: 10,
 		videoId: "D0xKqLheNpc",
 		title: "How to decide between quality and loading time of image on a website",
 		thumbnailPath: "",
-		url: "https://www.youtube.com/embed/D0xKqLheNpc",
+		url: "https://youtu.be/D0xKqLheNpc",
 		description: "Quick overview of what is like to deal with image on a responsive design",
-		type: "youtube"
+		type: POST_TYPES.YOUTUBE
 	},
 	{
-		id: 11,
 		videoId: "zny2DasAmq0",
 		title: "Drop.js Library | Create Html Drop-down, Tool-tips and Pop-ups",
 		thumbnailPath: "",
-		url: "https://www.youtube.com/embed/zny2DasAmq0",
+		url: "https://youtu.be/zny2DasAmq0",
 		description: "Quick introduction to Drop.js library",
-		type: "youtube"
+		type: POST_TYPES.YOUTUBE
+	},
+	{
+		videoId: null,
+		title: "Twitch loading mockup",
+		thumbnailPath: "./media/thumbnails/twitchLoading.gif",
+		url: "https://ecorreia45.github.io/Before-Semicolon/examples/twitchLoading/",
+		description: "This is a simple Twitch loading animation mock-up done in pure css.",
+		type: POST_TYPES.GITHUB
+	},
+	{
+		videoId: null,
+		title: "Search Field - Icon into text cursor",
+		thumbnailPath: "./media/thumbnails/searchField.gif",
+		url: "https://ecorreia45.github.io/Before-Semicolon/examples/searchField/",
+		description: "This is a clever usage of the search icon in a search field that transitions into a text cursor on field focus.",
+		type: POST_TYPES.GITHUB
+	},
+	{
+		videoId: null,
+		title: "Tabs indicator (example 1)",
+		thumbnailPath: "./media/thumbnails/tabsIndicator.gif",
+		url: "https://ecorreia45.github.io/Before-Semicolon/examples/tabsIndicator/",
+		description: "These tabs take advantage of linear-gradient and play on rotation to give them the nice tab look and appear as if they were shrinking or growing when active and inactive.",
+		type: POST_TYPES.GITHUB
+	},
+	{
+		videoId: null,
+		title: "Tabs indicator (example 2)",
+		thumbnailPath: "./media/thumbnails/tabsIndicator2.gif",
+		url: "https://ecorreia45.github.io/Before-Semicolon/examples/tabsIndicator2/",
+		description: "These tabs example uses same style of the example 1 but the tab indicator moves in the x-axis when tab is active while changing shape.",
+		type: POST_TYPES.GITHUB
+	},
+	{
+		videoId: null,
+		title: "Input label float",
+		thumbnailPath: "./media/thumbnails/inputLabelUp.gif",
+		url: "https://ecorreia45.github.io/Before-Semicolon/examples/inputLabelUp/",
+		description: "This is a clever usage of field and label. The label here moves above the field and stays there while the user interacts with the field.",
+		type: POST_TYPES.GITHUB
+	},
+	{
+		videoId: null,
+		title: "DropDown CSS Only!",
+		thumbnailPath: "./media/thumbnails/dropdown.gif",
+		url: "https://ecorreia45.github.io/Before-Semicolon/examples/dropdown/",
+		description: "This drop-down is build on css only and registers toggle and selected item in the drop-down.",
+		type: POST_TYPES.GITHUB
+	},
+	{
+		videoId: null,
+		title: "Tooltip CSS Only!",
+		thumbnailPath: "./media/thumbnails/tooltips.gif",
+		url: "https://ecorreia45.github.io/Before-Semicolon/examples/tooltips/",
+		description: "This is a tooltip example with very minimal html and simple css activated on hover and blur events.",
+		type: POST_TYPES.GITHUB
 	}
-]
+];
+
+posts.forEach((post, index) => {
+	post.id = index + 1; // avoid id of zero
+})
